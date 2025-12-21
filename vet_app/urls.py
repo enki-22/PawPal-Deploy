bfff8b4fb27bda24ea2263be406673858532fe8c
enki <enki.prince.alava@gmail.com>
2025-12-21 20:27:39 +0800

    added imports for urls

--- vet_app/urls.py
+++ vet_app/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from admin_panel.views_announcements import get_active_announcements
 from chatbot.views import symptom_checker_predict
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
